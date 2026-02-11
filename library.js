cbd8e7e7a3b126a5268fad08c62b12c49fe56b49
Emmanuel Oyelade <emmanueloyelade2@gmail.com>
2026-02-11 14:01:17 -0500

    Get rid of borders because I don't need them anymore

---
+++ library.js
@@ -0,0 +1,16 @@
+// const book = {
+//   title: "A book",
+//   author: "John Doe",
+//   pages: 100,
+//   status: "Not Read" 
+// };
+
+// const title = document.querySelector(".title");
+// const author = document.querySelector(".author");
+// const pages = document.querySelector(".pages");
+// const status = document.querySelector(".status");
+
+// title.textContent = book.title;
+// author.textContent = book.author;
+// pages.textContent = book.pages;
+// status.textContent = book.status;
