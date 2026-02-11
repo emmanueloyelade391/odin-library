e88d9d84346c0d3d21954ab461802d108c820175
Emmanuel Oyelade <emmanueloyelade2@gmail.com>
2026-02-11 14:45:52 -0500

    Write the logic of book creation and demo it
    The functions are going to create a book for the
    user and add it to the library list. I rewrote the
    logic for it manually to test out if the functions
    will work.

--- library.js
+++ library.js
@@ -1,16 +1,88 @@
-const book = {
+const myLibrary = [];
+
+function Book(title, author, pages, status) {
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status; 
+}
+
+function addBookToLibrary(title, author, pages, status) {
+  const newBook = new Book(title, author, pages, status);
+  myLibrary.push(newBook);
+
+  for (const book of myLibrary) {
+    const bookCard = document.createElement("div");
+    bookCard.classList = "book-card";
+
+    const title = document.createElement("p");
+    title.classList = "title";
+    title.textContent = book.title;
+    bookCard.appendChild(title);
+
+    const author = document.createElement("p");
+    author.classList = "author";
+    author.textContent = book.author;
+    bookCard.appendChild(author);
+
+    const pages = document.createElement("p");
+    pages.classList = "pages";
+    pages.textContent = book.pages;
+    bookCard.appendChild(pages);
+
+    const completed = document.createElement("p");
+    completed.classList = "completed";
+    completed.textContent = book.completed;
+    bookCard.appendChild(completed);
+
+    unreadBookContainer.appendChild(bookCard);
+  }
+}
+
+const unreadBookContainer = document.querySelector(".unread-book-container");
+
+const book1 = {
+  id: crypto.randomUUID(),
   title: "A book",
   author: "John Doe",
   pages: 100,
-  status: "Not Read" 
+  completed: "Not read",
 };
 
-const title = document.querySelector(".title");
-const author = document.querySelector(".author");
-const pages = document.querySelector(".pages");
-const status = document.querySelector(".status");
+const book2 = {
+  id: crypto.randomUUID(),
+  title: "Another book",
+  author: "John Doe",
+  pages: 200,
+  completed: "Read",
+};
 
-title.textContent = book.title;
-author.textContent = book.author;
-pages.textContent = book.pages;
-status.textContent = book.status;
+let bookList = [book1, book2];
+
+for (const book of bookList) {
+  const bookCard = document.createElement("div");
+  bookCard.classList = "book-card";
+
+  const title = document.createElement("p");
+  title.classList = "title";
+  title.textContent = book.title;
+  bookCard.appendChild(title);
+
+  const author = document.createElement("p");
+  author.classList = "author";
+  author.textContent = book.author;
+  bookCard.appendChild(author);
+
+  const pages = document.createElement("p");
+  pages.classList = "pages";
+  pages.textContent = book.pages;
+  bookCard.appendChild(pages);
+
+  const completed = document.createElement("p");
+  completed.classList = "completed";
+  completed.textContent = book.completed;
+  bookCard.appendChild(completed);
+
+  unreadBookContainer.appendChild(bookCard);
+}
